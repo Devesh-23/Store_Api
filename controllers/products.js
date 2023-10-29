92b0a304d337b1d143ed2fc515f5ab2cb13a5ab5
Devesh-23 <deveshpachauri23@gmail.com>
2023-10-29 20:06:17 +0530

    sorting in page

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,56 @@
-const getAllProductStatic = async (req, res) =>{
-    throw new Error('testing part')
-    res.status(200).json({msg: 'products testing route'})
+const product = require('../models/product')
+
+const getAllProductStatic = async (req, res) =>{    //this one is static to use that is we are giving the data to be searched hardcoded
+    //const search = 'oo'
+
+    const Products = await product.find({}).sort('-name price')
+    res.status(200).json({ nbHits:Products.length, Products })
 }
 
 const getAllProducts = async (req, res) =>{
-    res.status(200).json({msg: 'product route '})
+    // console.log(req.query)
+    const { featured, company, name, sort, fields } = req.query
+    const quertObj = {}; 
+    if(featured){
+        quertObj.featured = featured === 'true'?true:false;
+    }
+
+    if(company){
+        quertObj.company = company
+    }
+
+    if(name){
+        quertObj.name  = {$regex:name, $options:'i'}
+    }
+
+    // console.log(quertObj)
+    let result =  product.find(quertObj)
+
+    //sort
+    if(sort){
+        const spaceList = sort.split(',').join(' ')
+        result = result.sort(spaceList)
+        //console.log(sort)
+    }else{
+        result = result.sort('createdAt')
+    }
+
+
+    //select the particular fields
+    if(fields){
+        const fieldList = fields.split(',').join(' ')
+        result = result.select(fieldList)
+    } 
+    
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10 
+    const skip = (page-1)*limit
+
+    result = result.split(split).limit(limit)
+
+
+    const Products = await result
+    res.status(200).json({nbHits:Products.length, Products })
 }
 
 
